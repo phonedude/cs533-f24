9bb2b63fb08c205ba94e9785023a941b217de0f6
James Ecker <jim@jamess-mbp.lan>
2024-09-21 02:39:35 -0400

    Cleaned commented code out of server.js

--- assignments/Ecker/2/server.js
+++ assignments/Ecker/2/server.js
@@ -93,28 +93,6 @@ function buildRoutes() {
 // Build routes
 buildRoutes();
 
-/*
-// Bad Religion 
-app.get('/badreligion', setCookie('Artist', 'Bad Religion'), (request, response) => {
-    response.sendFile(path.join(__dirname, 'pages', 'badreligion.html'));
-});
-
-// Barney Miller
-app.get('/barneymiller', setCookie('TV Show', 'Barney Miller'), (request, response) => {
-    response.sendFile(path.join(__dirname, 'pages', 'barneymiller.html'));
-});
-
-// Perry Mason
-app.get('/perrymason', setCookie('Book Series', 'Perry Mason'), (request, response) => {
-    response.sendFile(path.join(__dirname, 'pages', 'perrymason.html'));
-});
-
-// Half Life
-app.get('/halflife', setCookie('Game Series', 'Half-Life'), (request, response) => {
-    response.sendFile(path.join(__dirname, 'pages', 'halflife.html'));
-});
-*/ 
-
 // Run Server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
