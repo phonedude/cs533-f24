12c371030b333ad83ef002f0666e0403e1555904
eagle787sf <45102291+eagle787sf@users.noreply.github.com>
2024-09-25 17:33:11 -0400

    Delete assignments/Rasmussen/1 directory

--- assignments/Rasmussen/1/node/my-first-async-io.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, fileContent) => {
-    if (err) {
-        return console.log(err)
-    }
-    const lines = fileContent.split('\n').length - 1;
-    console.log(lines);
-});
--- assignments/Rasmussen/1/node/my-first-io.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const lineCnt = fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1;
-console.log(lineCnt);
