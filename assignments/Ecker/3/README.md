15c614d4142c00dda9e3ddb496699d51b8d0b580
Jim Ecker <jim.ecker@gmail.com>
2024-10-18 17:17:28 -0400

    Added report generation and progress bar

---
+++ assignments/Ecker/3/README.md
@@ -0,0 +1,125 @@
+# Cookie Practices Report
+
+This report summarizes the cookie practices of 100 websites.
+
+| No. | URL | Final Status Code | Number of Cookies |
+|-----|-----|-------------------|-------------------|
+| 1 | http://000webhost.com | 403 | 0 |
+| 2 | http://4shared.com | 200 | 1 |
+| 3 | http://abcnews.go.com | 200 | 2 |
+| 4 | http://about.com | 200 | 0 |
+| 5 | http://accounts.google.com | 200 | 1 |
+| 6 | http://alexa.com | 200 | 0 |
+| 7 | http://amazon.co.uk | 200 | 0 |
+| 8 | http://amazon.es | 200 | 0 |
+| 9 | http://amazon.fr | 200 | 0 |
+| 10 | http://amebaownd.com | 500 | 0 |
+| 11 | http://android.com | 200 | 0 |
+| 12 | http://apache.org | 200 | 0 |
+| 13 | http://bandcamp.com | 200 | 5 |
+| 14 | http://bbc.co.uk | 200 | 0 |
+| 15 | http://berkeley.edu | 403 | 0 |
+| 16 | http://bigcommerce.com | 403 | 0 |
+| 17 | http://bloglovin.com | 403 | 0 |
+| 18 | http://bloomberg.com | 200 | 0 |
+| 19 | http://booking.com | 200 | 4 |
+| 20 | http://box.com | 200 | 1 |
+| 21 | http://bp0.blogger.com | 400 | 0 |
+| 22 | http://clickbank.net | 500 | 0 |
+| 23 | http://cloudflare.com | 403 | 2 |
+| 24 | http://deezer.com | 200 | 4 |
+| 25 | http://doi.org | 200 | 0 |
+| 26 | http://domainmarket.com | 403 | 0 |
+| 27 | http://e-monsite.com | 403 | 0 |
+| 28 | http://enable-javascript.com | 200 | 0 |
+| 29 | http://europa.eu | 200 | 0 |
+| 30 | http://eventbrite.com | 200 | 1 |
+| 31 | http://fb.me | 200 | 0 |
+| 32 | http://focus.de | 200 | 0 |
+| 33 | http://forms.gle | 400 | 0 |
+| 34 | http://ft.com | 200 | 2 |
+| 35 | http://ftc.gov | 200 | 0 |
+| 36 | http://gettyimages.com | 200 | 5 |
+| 37 | http://ggpht.com | 500 | 0 |
+| 38 | http://gmail.com | 200 | 1 |
+| 39 | http://godaddy.com | 403 | 3 |
+| 40 | http://gofundme.com | 200 | 2 |
+| 41 | http://google.co.uk | 200 | 2 |
+| 42 | http://google.pl | 200 | 2 |
+| 43 | http://groups.google.com | 200 | 2 |
+| 44 | http://gstatic.com | 404 | 0 |
+| 45 | http://guardian.co.uk | 200 | 2 |
+| 46 | http://ig.com.br | 403 | 1 |
+| 47 | http://imdb.com | 403 | 0 |
+| 48 | http://independent.co.uk | 200 | 9 |
+| 49 | http://ja.wikipedia.org | 200 | 5 |
+| 50 | http://jhu.edu | 200 | 0 |
+| 51 | http://jstor.org | 200 | 6 |
+| 52 | http://justice.gov | 200 | 0 |
+| 53 | http://last.fm | 200 | 6 |
+| 54 | http://lavanguardia.com | 200 | 0 |
+| 55 | http://linktr.ee | 500 | 4 |
+| 56 | http://mail.ru | 200 | 4 |
+| 57 | http://merriam-webster.com | 200 | 0 |
+| 58 | http://myaccount.google.com | 200 | 1 |
+| 59 | http://myspace.com | 200 | 4 |
+| 60 | http://namecheap.com | 403 | 0 |
+| 61 | http://naver.com | 200 | 0 |
+| 62 | http://netflix.com | 200 | 4 |
+| 63 | http://netvibes.com | 200 | 1 |
+| 64 | http://news.google.com | 200 | 2 |
+| 65 | http://nginx.com | 200 | 9 |
+| 66 | http://nih.gov | 500 | 0 |
+| 67 | http://opera.com | 200 | 0 |
+| 68 | http://photobucket.com | 200 | 0 |
+| 69 | http://photos1.blogger.com | 404 | 0 |
+| 70 | http://plus.google.com | 200 | 0 |
+| 71 | http://prezi.com | 200 | 2 |
+| 72 | http://public-api.wordpress.com | 200 | 0 |
+| 73 | http://redbull.com | 200 | 1 |
+| 74 | http://sedoparking.com | 200 | 0 |
+| 75 | http://softonic.com | 200 | 0 |
+| 76 | http://spiegel.de | 200 | 0 |
+| 77 | http://ssl-images-amazon.com | 500 | 0 |
+| 78 | http://stanford.edu | 200 | 0 |
+| 79 | http://steampowered.com | 200 | 3 |
+| 80 | http://t-online.de | 200 | 0 |
+| 81 | http://techcrunch.com | 200 | 0 |
+| 82 | http://theglobeandmail.com | 200 | 2 |
+| 83 | http://thetimes.co.uk | 200 | 4 |
+| 84 | http://tinyurl.com | 200 | 5 |
+| 85 | http://tmz.com | 200 | 0 |
+| 86 | http://trustpilot.com | 200 | 0 |
+| 87 | http://usnews.com | 500 | 0 |
+| 88 | http://variety.com | 200 | 0 |
+| 89 | http://video.google.com | 200 | 2 |
+| 90 | http://vimeo.com | 200 | 2 |
+| 91 | http://washington.edu | 200 | 0 |
+| 92 | http://washingtonpost.com | 200 | 9 |
+| 93 | http://webmd.com | 403 | 2 |
+| 94 | http://whatsapp.com | 200 | 3 |
+| 95 | http://wikia.com | 200 | 5 |
+| 96 | http://wikihow.com | 200 | 3 |
+| 97 | http://wordpress.org | 200 | 0 |
+| 98 | http://workspace.google.com | 200 | 1 |
+| 99 | http://www.canalblog.com | 200 | 0 |
+| 100 | http://www.over-blog.com | 200 | 0 |
+
+## Summary Statistics
+
+- **Total Cookies**: 142
+- **Min Cookies per Site**: 0
+- **Max Cookies per Site**: 9
+- **Mean Cookies per Site**: 1.42
+- **Median Cookies per Site**: 0.00
+
+## Cookie Attribute Counts
+
+- **HttpOnly Cookies**: 0
+- **Secure Cookies**: 91
+- **SameSite Cookies**: 4
+  - **SameSite=Strict**: 0
+  - **SameSite=Lax**: 4
+  - **SameSite=None**: 0
+- **Cookies with Path Attribute**: 142
+  - **Path not '/'**: 0
--- assignments/Ecker/3/report.pl
+++ assignments/Ecker/3/report.pl
@@ -5,6 +5,9 @@ use LWP::UserAgent;
 use HTTP::Cookies;
 use IO::Socket::SSL;
 use Mozilla::CA;
+use List::Util qw(min max sum);
+use POSIX qw(floor);
+use Term::ProgressBar::Simple;
 
 # Check if filename is provided as an argument
 if (@ARGV != 1) {
@@ -15,10 +18,39 @@ if (@ARGV != 1) {
 my $filename = $ARGV[0];
 open(my $fh, '<', $filename) or die "Could not open file '$filename' $!";
 
+# Read all URLs into an array
+my @urls;
 while (my $url = <$fh>) {
     chomp $url;
     next unless $url; # Skip empty lines
+    push @urls, $url;
+}
+close($fh);
 
+# Initialize progress bar
+my $num_urls = scalar @urls;
+
+# Output file name and number of URLs
+print "Processing file: $filename\n";
+print "Number of URLs: $num_urls\n";
+
+my $progress = Term::ProgressBar::Simple->new($num_urls);
+
+# Arrays to store data for summary statistics
+my @num_cookies_list;
+my @site_data; # Array of hashes for each site's data
+my %cookie_attribute_counts = (
+    HttpOnly        => 0,
+    Secure          => 0,
+    SameSite        => 0,
+    SameSite_Strict => 0,
+    SameSite_Lax    => 0,
+    SameSite_None   => 0,
+    Path            => 0,
+    Path_NotRoot    => 0,
+);
+
+foreach my $url (@urls) {
     # Add 'http://' if the URL doesn't have a scheme
     unless ($url =~ m{^https?://}) {
         $url = "http://$url";
@@ -33,6 +65,7 @@ while (my $url = <$fh>) {
             SSL_verify_mode => IO::Socket::SSL::SSL_VERIFY_NONE,
             verify_hostname => 0,
         },
+        timeout => 20,
     );
 
     my @responses;
@@ -52,55 +85,120 @@ while (my $url = <$fh>) {
     # Perform the request
     my $response = $ua->get($url);
 
-    # Check for request errors
-    if ($response->is_error) {
-        print "URL: $url\n";
-        print "Error: " . $response->status_line . "\n";
-        print "----------------------------------------\n\n";
-        next;
-    }
-
     # Get the final response code
     my $final_code = $response->code;
 
-    # Print the collected data
-    print "URL: $url\n";
-    print "Final Response Code: $final_code\n\n";
-
-    if (@responses) {
-        print "Responses and Cookies Along the Way:\n";
-        foreach my $resp (@responses) {
-            print "Response Code: $resp->{code}\n";
-            if (@{$resp->{cookies}}) {
-                print "Cookies set in this response:\n";
-                foreach my $cookie (@{$resp->{cookies}}) {
-                    print "\t$cookie\n";
-                }
-            } else {
-                print "No cookies set in this response.\n";
-            }
-            print "\n";
-        }
-    } else {
-        print "No intermediate responses.\n";
-    }
-
-    # Optionally, print the final cookies stored in the cookie jar
+    # Collect cookie data
     my @all_cookies;
     $cookie_jar->scan(sub {
-        my ($version, $key, $val) = @_;
-        push @all_cookies, "$key=$val";
+        my ($version, $key, $val, $path, $domain, $port, $path_spec, $secure, $expires, $discard, $hash) = @_;
+        push @all_cookies, {
+            key       => $key,
+            value     => $val,
+            path      => $path,
+            secure    => $secure,
+            expires   => $expires,
+            discard   => $discard,
+            httponly  => $hash->{httponly},
+            samesite  => $hash->{samesite},
+        };
     });
 
-    if (@all_cookies) {
-        print "Final Cookie Jar Contents:\n";
-        foreach my $cookie (@all_cookies) {
-            print "\t$cookie\n";
+    my $num_cookies = scalar @all_cookies;
+    push @num_cookies_list, $num_cookies;
+
+    # Analyze cookie attributes
+    foreach my $cookie (@all_cookies) {
+        # Count HttpOnly
+        if ($cookie->{httponly}) {
+            $cookie_attribute_counts{HttpOnly}++;
         }
-    } else {
-        print "Final Cookie Jar is empty.\n";
+
+        # Count Secure
+        if ($cookie->{secure}) {
+            $cookie_attribute_counts{Secure}++;
+        }
+
+        # Count Path
+        if (defined $cookie->{path}) {
+            $cookie_attribute_counts{Path}++;
+            if ($cookie->{path} ne '/') {
+                $cookie_attribute_counts{Path_NotRoot}++;
+            }
+        }
+
+        # Count SameSite
+        if (defined $cookie->{samesite}) {
+            $cookie_attribute_counts{SameSite}++;
+            if (lc $cookie->{samesite} eq 'strict') {
+                $cookie_attribute_counts{SameSite_Strict}++;
+            } elsif (lc $cookie->{samesite} eq 'lax') {
+                $cookie_attribute_counts{SameSite_Lax}++;
+            } elsif (lc $cookie->{samesite} eq 'none') {
+                $cookie_attribute_counts{SameSite_None}++;
+            }
+        }
     }
-    print "----------------------------------------\n\n";
+
+    # Store site data
+    push @site_data, {
+        url          => $url,
+        final_code   => $final_code,
+        num_cookies  => $num_cookies,
+    };
+
+    # Update the progress bar
+    $progress++;
 }
 
-close($fh);
+# Generate Markdown table
+open(my $md_fh, '>', 'README.md') or die "Could not open 'README.md' $!";
+
+print $md_fh "# Cookie Practices Report\n\n";
+print $md_fh "This report summarizes the cookie practices of $num_urls websites.\n\n";
+
+# Table Header
+print $md_fh "| No. | URL | Final Status Code | Number of Cookies |\n";
+print $md_fh "|-----|-----|-------------------|-------------------|\n";
+
+# Table Rows
+my $counter = 1;
+foreach my $site (@site_data) {
+    print $md_fh "| $counter | $site->{url} | $site->{final_code} | $site->{num_cookies} |\n";
+    $counter++;
+}
+
+# Summary Statistics
+my $total_cookies = sum(@num_cookies_list);
+my $min_cookies = min(@num_cookies_list);
+my $max_cookies = max(@num_cookies_list);
+my $mean_cookies = $total_cookies / scalar(@num_cookies_list);
+my @sorted_cookies = sort { $a <=> $b } @num_cookies_list;
+my $median_cookies;
+if (@sorted_cookies % 2) {
+    $median_cookies = $sorted_cookies[int(@sorted_cookies/2)];
+} else {
+    my $mid = @sorted_cookies / 2;
+    $median_cookies = ($sorted_cookies[$mid - 1] + $sorted_cookies[$mid]) / 2;
+}
+
+print $md_fh "\n## Summary Statistics\n\n";
+print $md_fh "- **Total Cookies**: $total_cookies\n";
+print $md_fh "- **Min Cookies per Site**: $min_cookies\n";
+print $md_fh "- **Max Cookies per Site**: $max_cookies\n";
+printf $md_fh "- **Mean Cookies per Site**: %.2f\n", $mean_cookies;
+printf $md_fh "- **Median Cookies per Site**: %.2f\n\n", $median_cookies;
+
+print $md_fh "## Cookie Attribute Counts\n\n";
+print $md_fh "- **HttpOnly Cookies**: $cookie_attribute_counts{HttpOnly}\n";
+print $md_fh "- **Secure Cookies**: $cookie_attribute_counts{Secure}\n";
+print $md_fh "- **SameSite Cookies**: $cookie_attribute_counts{SameSite}\n";
+print $md_fh "  - **SameSite=Strict**: $cookie_attribute_counts{SameSite_Strict}\n";
+print $md_fh "  - **SameSite=Lax**: $cookie_attribute_counts{SameSite_Lax}\n";
+print $md_fh "  - **SameSite=None**: $cookie_attribute_counts{SameSite_None}\n";
+print $md_fh "- **Cookies with Path Attribute**: $cookie_attribute_counts{Path}\n";
+print $md_fh "  - **Path not '/'**: $cookie_attribute_counts{Path_NotRoot}\n";
+
+close($md_fh);
+
+print "\nReport generated in 'README.md'\n";
