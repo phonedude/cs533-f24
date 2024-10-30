3c0bc50d741a9486e30e3cebeee5f4015c1ab4d5
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2024-10-29 22:23:35 -0400

    get trivial json return

--- assignments/Rasmussen/4/server.js
+++ assignments/Rasmussen/4/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const axios = require('axios');
 
 const app = express();
 const port = 4000;
@@ -10,6 +12,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route for the root URL
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Function to check if a website is frameable
+const isFrameable = async (url) => {
+    try {
+        const response = await axios.get(url);
+        return !response.headers['x-frame-options'];
+    } catch (error) {
+        console.error(`Error checking ${url}:`, error.message);
+        return false;
+    }
+};
+
+// Route to check frameability of websites
+app.get('/check-frameable', async (req, res) => {
+    const filePath = path.join(__dirname, 'frameable', './ARASM002_test');
+    const websites = fs.readFileSync(filePath, 'utf-8').split('\n').filter(Boolean);
+
+    const results = await Promise.all(websites.map(async (website) => {
+        const frameable = await isFrameable(website);
+        return { website, frameable };
+    }));
+
+    res.json(results);
 });
 
 // Start the server
