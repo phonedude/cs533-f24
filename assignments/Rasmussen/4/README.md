6642632425de527986c653db30e7790a95ecc250
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2024-10-30 21:59:36 -0400

    Everything but extra credit

--- assignments/Rasmussen/4/README.md
+++ assignments/Rasmussen/4/README.md
@@ -1,7 +1,156 @@
 
+## Assignment 4, CS 433/533 Web Security, Fall 2024
+### Anton Rasmussen
+
+For this assignment, we explored website framing vulnerabilities by testing the frameability of 100 popular websites and demonstrating a cookie theft attack using iframes.
+
+The project consists of two main components:
+
+1. A website framing test that checks whether popular websites can be embedded in iframes, documenting the reasons why some sites cannot be framed (e.g. X-Frame-Options headers, Content-Security-Policy).
+
+2. A demonstration of a cookie theft vulnerability using iframes, showing how cookies scoped only with the Path attribute can be accessed by malicious pages.
+
+The project includes:
+- A main index page with links to both demonstrations
+- Individual HTML pages for each tested website showing whether it can be framed
+- An attacker page that attempts to steal cookies from a vulnerable page
+- A vulnerable page that sets cookies with only path-based protection
+
+After completing this project we have the following directory structure:
+
+```
+.
+├── README.md
+├── data
+│   ├── ARASM002@ODU.EDU
+│   └── ARASM002_test
+├── frame-path-attack
+│   ├── attacker-page
+│   │   └── attacker.html
+│   └── vulnerable-page
+│       └── vulnerable.html
+├── frameable
+│   ├── 4shared.com.html
+│   ├── abcnews.go.com.html
+│   ├── alibaba.com.html
+│   ├── aliexpress.com.html
+│   ├── aol.com.html
+│   ├── apache.org.html
+│   ├── apple.com.html
+│   ├── arxiv.org.html
+│   ├── biblegateway.com.html
+│   ├── biglobe.ne.jp.html
+│   ├── bloomberg.com.html
+│   ├── booking.com.html
+│   ├── britannica.com.html
+│   ├── buzzfeed.com.html
+│   ├── cambridge.org.html
+│   ├── cnil.fr.html
+│   ├── cnn.com.html
+│   ├── cointernet.com.co.html
+│   ├── com.com.html
+│   ├── cpanel.net.html
+│   ├── discord.com.html
+│   ├── disqus.com.html
+│   ├── doi.org.html
+│   ├── drive.google.com.html
+│   ├── dropbox.com.html
+│   ├── ea.com.html
+│   ├── elmundo.es.html
+│   ├── espn.com.html
+│   ├── feedburner.com.html
+│   ├── forms.gle.html
+│   ├── g.co.html
+│   ├── get.google.com.html
+│   ├── gfycat.com.html
+│   ├── globo.com.html
+│   ├── godaddy.com.html
+│   ├── gofundme.com.html
+│   ├── goo.ne.jp.html
+│   ├── goodreads.com.html
+│   ├── google.ru.html
+│   ├── gravatar.com.html
+│   ├── gsmarena.com.html
+│   ├── guardian.co.uk.html
+│   ├── hatena.ne.jp.html
+│   ├── hindustantimes.com.html
+│   ├── hp.com.html
+│   ├── ign.com.html
+│   ├── ikea.com.html
+│   ├── imageshack.us.html
+│   ├── independent.co.uk.html
+│   ├── jhu.edu.html
+│   ├── jstor.org.html
+│   ├── justgiving.com.html
+│   ├── latimes.com.html
+│   ├── liberation.fr.html
+│   ├── linkedin.com.html
+│   ├── mailchimp.com.html
+│   ├── marca.com.html
+│   ├── naver.com.html
+│   ├── news.com.au.html
+│   ├── npr.org.html
+│   ├── nytimes.com.html
+│   ├── offset.com.html
+│   ├── oup.com.html
+│   ├── outlook.com.html
+│   ├── ovhcloud.com.html
+│   ├── people.com.html
+│   ├── php.net.html
+│   ├── pinterest.fr.html
+│   ├── pl.wikipedia.org.html
+│   ├── play.google.com.html
+│   ├── playstation.com.html
+│   ├── plos.org.html
+│   ├── prezi.com.html
+│   ├── pt.wikipedia.org.html
+│   ├── reverbnation.com.html
+│   ├── sakura.ne.jp.html
+│   ├── samsung.com.html
+│   ├── search.yahoo.com.html
+│   ├── sina.com.cn.html
+│   ├── spiegel.de.html
+│   ├── support.google.com.html
+│   ├── thefreedictionary.com.html
+│   ├── theverge.com.html
+│   ├── usgs.gov.html
+│   ├── vistaprint.com.html
+│   ├── walmart.com.html
+│   ├── webmd.com.html
+│   ├── webnode.page.html
+│   ├── whitehouse.gov.html
+│   ├── wikimedia.org.html
+│   ├── wordpress.org.html
+│   ├── wp.com.html
+│   ├── www.gov.uk.html
+│   ├── www.over-blog.com.html
+│   ├── www.wix.com.html
+│   ├── www.yahoo.com.html
+│   ├── yadi.sk.html
+│   ├── ytimg.com.html
+│   ├── zendesk.com.html
+│   └── zippyshare.com.html
+├── index.html
+├── package-lock.json
+├── package.json
+├── server.js
+└── templates
+    ├── frameable_template.html
+    └── index_template.html
+```
+
+Note: I ran tree -I 'node_modules' to ignore the verbose node_modules directory; notably this directory is hidden (i.e. configured to be ignored by git).
+
+
+### The videos demonstrating each of these tasks can be found here:
+
+- [Which public sites are framable?](https://youtu.be/RbsX8UF_SOQ)
+- [Frame Path attack](https://youtu.be/E4ytD1ksskY)
+
+
 # Website Rendering Results
 
-## Frameable Websites (29)
+## Frameable Websites (30)
 - http://4shared.com
 - http://abcnews.go.com
 - http://biblegateway.com
@@ -30,9 +179,10 @@
 - http://webmd.com
 - http://wikimedia.org
 - http://www.wix.com
+- http://yadi.sk
 - http://zippyshare.com
 
-## Not Frameable Websites (71)
+## Not Frameable Websites (70)
 - [http://alibaba.com](frameable/alibaba.com.html) (Reason: X-Frame-Options)
 - [http://aliexpress.com](frameable/aliexpress.com.html) (Reason: Error)
 - [http://aol.com](frameable/aol.com.html) (Reason: X-Frame-Options)
@@ -73,7 +223,7 @@
 - [http://jstor.org](frameable/jstor.org.html) (Reason: X-Frame-Options)
 - [http://justgiving.com](frameable/justgiving.com.html) (Reason: X-Frame-Options)
 - [http://latimes.com](frameable/latimes.com.html) (Reason: X-Frame-Options)
-- [http://linkedin.com](frameable/linkedin.com.html) (Reason: Error)
+- [http://linkedin.com](frameable/linkedin.com.html) (Reason: X-Frame-Options)
 - [http://mailchimp.com](frameable/mailchimp.com.html) (Reason: X-Frame-Options)
 - [http://naver.com](frameable/naver.com.html) (Reason: X-Frame-Options)
 - [http://nytimes.com](frameable/nytimes.com.html) (Reason: X-Frame-Options)
@@ -101,7 +251,6 @@
 - [http://www.gov.uk](frameable/www.gov.uk.html) (Reason: X-Frame-Options)
 - [http://www.over-blog.com](frameable/www.over-blog.com.html) (Reason: X-Frame-Options)
 - [http://www.yahoo.com](frameable/www.yahoo.com.html) (Reason: X-Frame-Options)
-- [http://yadi.sk](frameable/yadi.sk.html) (Reason: Content-Security-Policy)
 - [http://ytimg.com](frameable/ytimg.com.html) (Reason: Error)
 - [http://zendesk.com](frameable/zendesk.com.html) (Reason: Content-Security-Policy)
     
--- assignments/Rasmussen/4/server.js
+++ assignments/Rasmussen/4/server.js
@@ -16,30 +16,23 @@ app.get('/', (req, res) => {
 
 // Route for the vulnerable page
 app.get('/frame-path-attack/vulnerable-page', (req, res) => {
-    // Remove X-Frame-Options header to ensure framing works
-    res.removeHeader('X-Frame-Options');
-    
-    // Set multiple cookies to increase chances of demonstration
-    res.cookie('sensitiveData', 'secret123', {
+
+    // Set a cookie that's only protected by Path attribute
+    res.cookie('serverSecret', 'super_sensitive_data', {
         path: '/frame-path-attack/vulnerable-page',
-        httpOnly: false,
-        sameSite: 'Lax'  // Changed to Lax which is more permissive than Strict
+        httpOnly: false // Allow JavaScript access
     });
-    
-    res.cookie('sessionId', 'demo-session-12345', {
-        path: '/frame-path-attack/vulnerable-page',
-        httpOnly: false,
-        sameSite: 'Lax'
-    });
-    
-    // Add headers to explicitly allow framing
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     
     res.sendFile(path.join(__dirname, 'frame-path-attack/vulnerable-page', 'vulnerable.html'));
 });
 // Route for the attacker page
 app.get('/frame-path-attack/attacker-page', (req, res) => {
+    // Set a cookie for the attacker page path to demonstrate cookie isolation
+    res.cookie('attackerCookie', 'not-sensitive', {
+        path: '/frame-path-attack/attacker-page',
+        httpOnly: false
+    });
     res.sendFile(path.join(__dirname, 'frame-path-attack/attacker-page', 'attacker.html'));
 });
 
@@ -139,6 +132,155 @@ app.get('/check-frameable', async (req, res) => {
 
     // Update README.md
     const readmeContent = `
+## Assignment 4, CS 433/533 Web Security, Fall 2024
+### Anton Rasmussen
+
+For this assignment, we explored website framing vulnerabilities by testing the frameability of 100 popular websites and demonstrating a cookie theft attack using iframes.
+
+The project consists of two main components:
+
+1. A website framing test that checks whether popular websites can be embedded in iframes, documenting the reasons why some sites cannot be framed (e.g. X-Frame-Options headers, Content-Security-Policy).
+
+2. A demonstration of a cookie theft vulnerability using iframes, showing how cookies scoped only with the Path attribute can be accessed by malicious pages.
+
+The project includes:
+- A main index page with links to both demonstrations
+- Individual HTML pages for each tested website showing whether it can be framed
+- An attacker page that attempts to steal cookies from a vulnerable page
+- A vulnerable page that sets cookies with only path-based protection
+
+After completing this project we have the following directory structure:
+
+\`\`\`
+.
+├── README.md
+├── data
+│   ├── ARASM002@ODU.EDU
+│   └── ARASM002_test
+├── frame-path-attack
+│   ├── attacker-page
+│   │   └── attacker.html
+│   └── vulnerable-page
+│       └── vulnerable.html
+├── frameable
+│   ├── 4shared.com.html
+│   ├── abcnews.go.com.html
+│   ├── alibaba.com.html
+│   ├── aliexpress.com.html
+│   ├── aol.com.html
+│   ├── apache.org.html
+│   ├── apple.com.html
+│   ├── arxiv.org.html
+│   ├── biblegateway.com.html
+│   ├── biglobe.ne.jp.html
+│   ├── bloomberg.com.html
+│   ├── booking.com.html
+│   ├── britannica.com.html
+│   ├── buzzfeed.com.html
+│   ├── cambridge.org.html
+│   ├── cnil.fr.html
+│   ├── cnn.com.html
+│   ├── cointernet.com.co.html
+│   ├── com.com.html
+│   ├── cpanel.net.html
+│   ├── discord.com.html
+│   ├── disqus.com.html
+│   ├── doi.org.html
+│   ├── drive.google.com.html
+│   ├── dropbox.com.html
+│   ├── ea.com.html
+│   ├── elmundo.es.html
+│   ├── espn.com.html
+│   ├── feedburner.com.html
+│   ├── forms.gle.html
+│   ├── g.co.html
+│   ├── get.google.com.html
+│   ├── gfycat.com.html
+│   ├── globo.com.html
+│   ├── godaddy.com.html
+│   ├── gofundme.com.html
+│   ├── goo.ne.jp.html
+│   ├── goodreads.com.html
+│   ├── google.ru.html
+│   ├── gravatar.com.html
+│   ├── gsmarena.com.html
+│   ├── guardian.co.uk.html
+│   ├── hatena.ne.jp.html
+│   ├── hindustantimes.com.html
+│   ├── hp.com.html
+│   ├── ign.com.html
+│   ├── ikea.com.html
+│   ├── imageshack.us.html
+│   ├── independent.co.uk.html
+│   ├── jhu.edu.html
+│   ├── jstor.org.html
+│   ├── justgiving.com.html
+│   ├── latimes.com.html
+│   ├── liberation.fr.html
+│   ├── linkedin.com.html
+│   ├── mailchimp.com.html
+│   ├── marca.com.html
+│   ├── naver.com.html
+│   ├── news.com.au.html
+│   ├── npr.org.html
+│   ├── nytimes.com.html
+│   ├── offset.com.html
+│   ├── oup.com.html
+│   ├── outlook.com.html
+│   ├── ovhcloud.com.html
+│   ├── people.com.html
+│   ├── php.net.html
+│   ├── pinterest.fr.html
+│   ├── pl.wikipedia.org.html
+│   ├── play.google.com.html
+│   ├── playstation.com.html
+│   ├── plos.org.html
+│   ├── prezi.com.html
+│   ├── pt.wikipedia.org.html
+│   ├── reverbnation.com.html
+│   ├── sakura.ne.jp.html
+│   ├── samsung.com.html
+│   ├── search.yahoo.com.html
+│   ├── sina.com.cn.html
+│   ├── spiegel.de.html
+│   ├── support.google.com.html
+│   ├── thefreedictionary.com.html
+│   ├── theverge.com.html
+│   ├── usgs.gov.html
+│   ├── vistaprint.com.html
+│   ├── walmart.com.html
+│   ├── webmd.com.html
+│   ├── webnode.page.html
+│   ├── whitehouse.gov.html
+│   ├── wikimedia.org.html
+│   ├── wordpress.org.html
+│   ├── wp.com.html
+│   ├── www.gov.uk.html
+│   ├── www.over-blog.com.html
+│   ├── www.wix.com.html
+│   ├── www.yahoo.com.html
+│   ├── yadi.sk.html
+│   ├── ytimg.com.html
+│   ├── zendesk.com.html
+│   └── zippyshare.com.html
+├── index.html
+├── package-lock.json
+├── package.json
+├── server.js
+└── templates
+    ├── frameable_template.html
+    └── index_template.html
+\`\`\`
+
+Note: I ran tree -I 'node_modules' to ignore the verbose node_modules directory; notably this directory is hidden (i.e. configured to be ignored by git).
+
+
+### The videos demonstrating each of these tasks can be found here:
+
+- [Which public sites are framable?](https://youtu.be/RbsX8UF_SOQ)
+- [Frame Path attack](https://youtu.be/E4ytD1ksskY)
+
+
 # Website Rendering Results
 
 ## Frameable Websites (${frameableCount})
